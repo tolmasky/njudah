eb9aa8a591b52dd91ec9a1190fe628d5683615c1
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2017-04-13 22:34:10 -0700

    Update version.
    Reviewed by @tolmasky.

---
+++ pre-publish/update-version.js
@@ -0,0 +1,27 @@
+const version = process.argv[2];
+const fs = require("fs");
+const packages = fs.readdirSync("..")
+    .map(aPath => path.join(__dirname, "..", aPath))
+    .filter(aPath => path.basename(aPath).charAt(0) !== "." && fs.lstatSync(aPath).isDirectory());
+
+const packageJSONs = packages.map(aPackagePath => require(path.join(aPackagePath, "package.json")));
+const upgradedPackageJSONs = packageJSONs.map(aPackageJSON => Object.assign({ },
+    aPackageJSON,
+    { "version": version },
+    upgradeDependencies(aPackageJSON, "dependencies"),
+    upgradeDependencies(aPackageJSON, "peerDependencies")));
+
+packages.forEach((aPath, anIndex) =>
+    fs.writeFileSync(path.join(aPath, "package.json"), JSON.stringify(upgradedPackageJSONs[anIndex], null, 2), "utf-8"))
+
+function upgradeDependencies(aPackageJSON, aKey)
+{
+    if (!aPackageJSON[aKey])
+        return;
+
+    const dependencies = aPackageJSON[aKey];
+    const names = Object.keys(dependencies);
+
+    return { [aKey]: names.reduce((accumulator, key) =>
+        Object.assign(accumulator, { [key]: key.indexOf("@njudah/") === 0 ? version : dependencies[key] }), { }) };
+}
